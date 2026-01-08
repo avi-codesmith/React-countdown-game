11cf7e3f3190adfbb0c5981ceba395e783114830
avi-codesmith <avi.codesmith@gmail.com>
2026-01-08 18:52:14 +0530

    Make header component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,30 @@
+import { useState } from "react";
+
 export default function Player() {
+  const [userName, setUserName] = useState(null);
+  const [btnClick, setBtnClick] = useState(false);
+
+  function handleUserName(event) {
+    setBtnClick(false);
+    setUserName(event.target.value);
+  }
+
+  function handleClick() {
+    setBtnClick(true);
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {btnClick ? userName : "there!"}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input
+          type="text"
+          value={btnClick ? "" : userName}
+          onChange={(event) => handleUserName(event)}
+        />
+        <button disabled={!userName} onClick={handleClick}>
+          Set Name
+        </button>
       </p>
     </section>
   );
