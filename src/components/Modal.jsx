51630a9a0fee8e5882d134d10534a0f7a8e7512f
avi-codesmith <avi.codesmith@gmail.com>
2026-01-09 22:55:41 +0530

    Project complete

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,17 @@
 import { useImperativeHandle, useRef } from "react";
 
-export default function Modal({ result, targetTime, ref }) {
+export default function Modal({ timerReset, targetTime, ref, timeRemaining }) {
   const dialog = useRef();
+
+  const userLost = timeRemaining <= 0;
+  const formatedRemainingTime = (timeRemaining / 1000).toFixed(2);
+  const score = Math.round((1 - timeRemaining / (targetTime * 1000)) * 100);
+
+  // function handleEsc(event) {
+  //   if (event.key === "Escape") {
+  //     timerReset();
+  //   }
+  // }
 
   useImperativeHandle(ref, () => {
     return {
@@ -12,15 +22,36 @@ export default function Modal({ result, targetTime, ref }) {
   });
 
   return (
-    <dialog ref={dialog} className="result-modal">
-      <h2>You {result}</h2>
+    <dialog ref={dialog} className="result-modal" onClose={timerReset}>
+      {userLost ? <h2>😭 You have Lost</h2> : ""}
+      {!userLost ? (
+        <h2>
+          {score > 79 ? "👏 " : "😣"} Your Score: {score}
+        </h2>
+      ) : (
+        ""
+      )}
       <p>
-        The Target Time was <strong>{targetTime} seconds</strong>
+        The Target Time was{" "}
+        <strong>
+          {targetTime} second{targetTime > 1 ? "s" : ""}.
+        </strong>
       </p>
-      <p>
-        You stopped the timer with <strong>x</strong> second left.
-      </p>
-      <form method="dialog">
+      {formatedRemainingTime <= 0 ? (
+        <p>
+          You could not stop the timer with in{" "}
+          <strong>
+            {targetTime} second
+            {targetTime > 1 ? "s" : ""}.
+          </strong>
+        </p>
+      ) : (
+        <p>
+          You stopped the timer with <strong>{formatedRemainingTime}</strong>{" "}
+          second{formatedRemainingTime > 1 ? "s" : ""} left.
+        </p>
+      )}
+      <form method="dialog" onSubmit={timerReset}>
         <button>close</button>
       </form>
     </dialog>
--- src/components/TimerChallenges.jsx
+++ src/components/TimerChallenges.jsx
@@ -4,37 +4,53 @@ import Modal from "../components/Modal";
 export default function TimerChallenge({ title, targetTime }) {
   const timer = useRef();
   const dialog = useRef();
-  const [started, setStarted] = useState(false);
-  const [expired, setexpired] = useState(false);
+
+  const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
+
+  const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
+
+  if (timeRemaining <= 0) {
+    clearInterval(timer.current);
+    dialog.current.open();
+  }
 
   function handleStart() {
-    timer.current = setTimeout(() => {
-      setexpired(true);
-      dialog.current.open();
-    }, targetTime * 1000);
+    timer.current = setInterval(() => {
+      setTimeRemaining((prev) => {
+        return prev - 10;
+      });
+    }, 10);
+  }
 
-    setStarted(true);
+  function handleReset() {
+    setTimeRemaining(targetTime * 1000);
   }
 
   function handleStop() {
-    clearTimeout(timer.current);
+    dialog.current.open();
+    clearInterval(timer.current);
   }
 
   return (
     <>
-      <Modal ref={dialog} targetTime={targetTime} result="lost" />
+      <Modal
+        ref={dialog}
+        targetTime={targetTime}
+        timeRemaining={timeRemaining}
+        timerReset={handleReset}
+      />
       <section className="challenge">
         <h2>{title}</h2>
         <p className="challenge-time">
           {targetTime} second{targetTime > 1 ? "s" : ""}
         </p>
         <p>
-          <button onClick={started ? handleStop : handleStart}>
-            {started ? "Stop" : "Start"} Challenge
+          <button onClick={timerIsActive ? handleStop : handleStart}>
+            {timerIsActive ? "Stop" : "Start"} Challenge
           </button>
         </p>
-        <p className={started ? "active" : ""}>
-          {started ? (
+        <p className={timerIsActive ? "active" : ""}>
+          {timerIsActive ? (
             <span>
               <span className="spinner spin-once">⏳</span>{" "}
               <span>Timer is running...</span>
